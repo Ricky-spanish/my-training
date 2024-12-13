93191b6d12eca52cf4446183e787b77208833bfc
Ricky-spanish <PicklRick1988@gmail.com>
2024-12-14 00:52:32 +0300

    Add files via upload

---
+++ lesson 4.py
@@ -0,0 +1,13 @@
+#1st program
+print (9**0.5*5)
+#2an program
+print (type(9.99>9.98)(1000!=1000.1))
+#3rd program
+print (2*2+2)
+print (2*(2+2))
+print (2*2+2==(2+2)*2)
+#4th program
+print (str(123.456))
+print (int(123.456*10))
+
+
